83d754cf52f4bbcd0a9e47ce1bd99ea07ccab0fc
AIdar <aidar9500@gmail.com>
2019-07-22 18:10:29 +0500

    add events input radio

--- work_input/script.js
+++ work_input/script.js
@@ -1,4 +1,4 @@
-
+/*java script код для чекбокса*/
 function clickCheck() {
     if(checkbox.checked){
         alert("выбран ");
@@ -8,4 +8,19 @@ function clickCheck() {
 }
 var checkbox= document.getElementById("one");
 checkbox.onchange=clickCheck;
+/*java script код для радио кнопки*/
+
+function clickRadio() {
+    var radioTag= document.getElementsByName("radioX");
+    for(var i=0; i<radioTag.length; i++){
+
+        if(radioTag[i].checked){
+            alert("выбран "+  ++i + " элемент");
+        }
+    }
+
+}
+var buttonClick = document.getElementById("button__radio");
+buttonClick.onclick = clickRadio;
+
 
